32d498448476daed7dc8b55cc439d766b45e003f
Isaac Pressman <iwpressman@gmail.com>
2026-04-08 10:26:17 -0700

    Add: React basic infastructure

---
+++ packages/react-frontend/src/Table.jsx
@@ -0,0 +1,38 @@
+// src/Table.jsx
+function TableHeader() {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Remove</th>
+      </tr>
+    </thead>
+  );
+}
+
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
+    return (
+        <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+        </tr>
+    );
+   }
+  );
+}
+
+function Table(props) {
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={props.characterData} />
+      </table>
+    );
+}
+
+export default Table;
